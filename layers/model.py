5a53beb0a7c79e467520df64cb21c9aaad7727e3
909hehe <crypticahmed@gmail.com>
2023-12-23 04:09:27 +0530

    hmm

--- layers/model.py
+++ layers/model.py
@@ -19,32 +19,49 @@ class Model():
             layer.update(pre_layer.output)
             pre_layer = layer
 
+    def get_embed (self, logits):
+        temp = [0] * 10
+        temp[logits] = 1
+        return temp
+
     def backward_pass(self, logits):
         
-        curr_layer = self.output_layer
-
+        logits = self.get_embed (logits)
         grad_mat = [x - y for x, y in zip(self.output_layer.output, logits)]
         der_Z = [der_sigmoid(x) for x in self.output_layer.values]
 
-        delta_list = []
         del_curr = [x * y for x, y in zip(grad_mat, der_Z)]
-        delta_list.append(del_curr)
-        
+
+        curr_layer = self.output_layer
+
         while curr_layer != self.input_layer:
             
-            delta_post = delta_list[-1]
-            
-            mat = [neuron.weights for neuron in curr_layer.layer]
-            mat = np.array(mat)
-            mat = mat.transpose()
-            
-            der_Z = [der_sigmoid(x) for x in self.curr_layer.values]
-            
-            dot = np.dot(mat, delta_post)
-            del_curr = [x * y for x, y in zip(dot, der_Z)]
-            delta_list.append(del_curr)
+            del_curr = self.calc_delta(curr_layer, del_curr)
+
+            lr = 0.001
+            for i in range(len(curr_layer.layer)):
+                neuron = curr_layer.layer[i]
+
+                for j in range(len(neuron.weights)):
+                    
+                    delta = curr_layer.pre_layer.output[j] * del_curr[i]
+                    neuron.weights[j] = neuron.weights[j] - lr * delta
+
+            for j in range(len(del_curr)):
+                
+                neuron = curr_layer.layer[i]
+                neuron.bias = neuron.bias - lr * del_curr[i]
 
             curr_layer = curr_layer.pre_layer
+
+
+
+    def fit (self, X, Y, epochs):
+        for _ in range(epochs):
+            for i in range(len(X)):
+
+                self.forward_pass(X[i])
+                self.backward_pass(Y[i])
 
 
 
@@ -58,5 +75,18 @@ class Model():
             layers.append(curr_layer)
             curr_layer = curr_layer.pre_layer
 
-        # layers.append(curr_layer)
         return layers[::-1]
+    
+
+    def calc_delta(self, curr_layer, delta_post):
+
+        mat = [neuron.weights for neuron in curr_layer.layer]
+        mat = np.array(mat)
+        mat = mat.transpose()
+        
+        der_Z = [der_sigmoid(x) for x in self.curr_layer.values]
+        
+        dot = np.dot(mat, delta_post)
+        del_curr = [x * y for x, y in zip(dot, der_Z)]
+
+        return del_curr
