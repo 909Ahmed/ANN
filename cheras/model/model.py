e4fc610976ab754bfc4a628aa9433d50f877763d
Ahmed <140858364+909Ahmed@users.noreply.github.com>
2024-01-24 19:28:24 +0530

    Update model.py

--- cheras/model/model.py
+++ cheras/model/model.py
@@ -13,9 +13,9 @@ class Model():
         self.optimizer = None
 
 
-    def forward_pass (self, sample, Training=False):
+    def forward_pass (self, batch, Training=False):
 
-        activations = [sample]
+        activations = [batch]
         zlist = []
 
         for layer in self.layers:
@@ -25,53 +25,51 @@ class Model():
                 weights = [neuron.weights for neuron in layer.layer]
                 bias = [neuron.bias for neuron in layer.layer]
                 
-                zlist.append(np.add (np.dot(weights, activations[-1]), bias))
-                activations.append([sigmoid(z) for z in zlist[-1]])  
-            
+                pre_activations = []
+                pre_zlist = []
+
+                for i in range(len(batch)):
+
+                    pre_zlist.append(np.add (np.dot(weights, activations[-1][i]), bias))
+                    pre_activations.append([sigmoid(z) for z in pre_zlist[-1]])
+
+                    if layer.drop and Training:
+                            pre_zlist[-1] = drop_func(pre_zlist[-1], layer.drop)
+                            pre_activations[-1] = drop_func(pre_activations[-1], layer.drop)
+
+                zlist.append(pre_zlist)
+                activations.append(pre_activations)
+
             elif layer.type == 'conv':
                 
-                #aaaaargh 
-
-                zlist.append(np.add (np.dot(weights, activations[-1]), bias))
+                # Implementing Forward Pass for Convolutional Layers
+                zlist.append(np.add (np.dot(weights, activations[-2]), bias))
                 activations.append([sigmoid(z) for z in zlist[-1]])
-
-            elif layer.type == 'dropout' and Training:
-                
-                if zlist != []:
-
-                    zlist[-1] = drop_func(zlist[-1], layer.rate)
-                    activations[-1] = drop_func(activations[-1], layer.rate)
 
         return activations, zlist
 
 
     def backward_pass(self, activations, zlist, logits):
                 
-        grad_mat = cost_der(activations[-1], logits)
-        der_Z = [der_sigmoid(x) for x in zlist[-1]]
+        grad_mat = [cost_der(act, logit) for act, logit in zip(activations[-1], logits)]
+        der_Z = [[der_sigmoid(x) for x in pre_zlist] for pre_zlist in zlist[-1]] 
 
         del_curr = np.multiply(grad_mat, der_Z)
         del_w_list = []
         del_b_list = []
-        ign = 0
 
         for index, layer in enumerate(self.layers[::-1]):
-            
-            if layer.type == 'dropout':
-                
-                ign += 1
-                continue
 
-            prev_act = activations[-index - 2 + ign]
+            prev_act = activations[-index - 2]
             
-            del_weights = np.outer(del_curr, prev_act)
+            del_weights = [np.outer(del_curr_ele, prev_act_ele) for del_curr_ele, prev_act_ele in zip(del_curr, prev_act)]
             del_bias = np.array(del_curr)
             
             del_w_list.append(del_weights)
             del_b_list.append(del_bias)
 
             if (index < len(self.layers) - 1):
-                del_curr = calc_delta(layer.layer, del_curr, zlist[-index - 2 + ign])
+                del_curr = calc_delta(layer.layer, del_curr, zlist[-index - 2])
 
         return del_w_list, del_b_list
     
@@ -82,22 +80,22 @@ class Model():
                         
             for batch in range(len(X) // batch_size - 1):
                 
-                del_w = [np.zeros((layer.size, layer.pre_layer.size)) for layer in self.layers[::-1] if layer.type != 'dropout']
-                del_b = [np.zeros(layer.size) for layer in self.layers[::-1] if layer.type != 'dropout']
-                
-                for x, y in zip(X[batch_size * batch: batch_size * (batch+1)], Y[batch_size * batch: batch_size * (batch+1)]):
+                x = X[batch_size * batch: batch_size * (batch+1)]
+                y = Y[batch_size * batch: batch_size * (batch+1)]
+                y = list(y)
 
-                    activations, zlist = self.forward_pass(x, True)
-                    del_wb, del_bb = self.backward_pass(activations, zlist, get_embed(y, self.output_layer.size))
-                    
-                    del_w = [np.add(matA, matB) for matA, matB in zip(del_w, del_wb)]
-                    del_b = [np.add(listA, listB) for listA, listB in zip(del_b, del_bb)]
-
-                del_w = [matA / batch_size for matA in del_w]
-                del_b = [listA / batch_size for listA in del_b]
+                activations, zlist = self.forward_pass(x, True)
                 
-                self.update_weights(del_w, batch_size * epoch + batch)
-                self.update_bias(del_b, batch_size * epoch + batch)
+                del_wb, del_bb = self.backward_pass(activations, zlist, get_embed(y, self.output_layer.size))
+                
+                del_wb = [np.sum(del_wbb, axis=0) for del_wbb in del_wb]
+                del_bb = [np.sum(del_bbb, axis=0) for del_bbb in del_bb]
+
+                del_wb = [arr / batch_size for arr in del_wb]
+                del_bb = [arr / batch_size for arr in del_bb]
+
+                self.update_weights(del_wb, batch_size * epoch + batch)
+                self.update_bias(del_bb, batch_size * epoch + batch)
 
             self.evaluate (X_valid, Y_valid)
 
@@ -137,8 +135,13 @@ class Model():
     def evaluate (self, X_valid, Y_valid):
     
         count = 0
-
-        for x, y in zip(X_valid, Y_valid):
+        batch_size = 32 #hard_coder bolte
+        for batch in range(len(X_valid) // batch_size - 1):
+                
+            x = X_valid[batch_size * batch: batch_size * (batch+1)]
+            y = Y_valid[batch_size * batch: batch_size * (batch+1)]
+            y = list(y)
+            
             pred, temp = self.forward_pass(x)
             count += calc_acc(pred[-1], y)
         
@@ -159,3 +162,4 @@ class Model():
             curr_layer = curr_layer.pre_layer
         
         return layers[::-1]
+    
