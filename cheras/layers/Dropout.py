ee2ca3bca1b7b1ef0704703f495afee2fde74ca3
Ahmed <140858364+909Ahmed@users.noreply.github.com>
2024-02-04 08:48:45 +0530

    Delete cheras/layers/Dropout.py

--- cheras/layers/Dropout.py
+++
@@ -1,8 +0,0 @@
-class Dropout():
-
-    def __init__(self, rate, pre_layer):
-        
-        self.rate = rate
-        self.pre_layer = pre_layer
-        self.size = pre_layer.size
-        self.type = 'dropout'
